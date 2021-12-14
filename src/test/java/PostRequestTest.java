0936e39d65b1014aefb0c0f7ccea5c5e41b3cc91
UBCh <u2b127@gmail.ru>
2021-12-14 11:34:06 +0300

    It's sunny in California

--- src/test/java/PostRequestTest.java
+++ src/test/java/PostRequestTest.java
@@ -10,7 +10,7 @@ public class PostRequestTest {
     void shouldPostRequest() {
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data")
+                .body("some value")
                 .when()
                 .post("/post")
 
