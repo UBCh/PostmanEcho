9d6f5fa97d797111d22a6660d9a167f4c69363a5
UBCh <u2b127@gmail.ru>
2021-12-14 10:59:12 +0300

    initialization of the project

---
+++ src/test/java/PostRequestTest.java
@@ -0,0 +1,22 @@
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostRequestTest {
+
+    @Test
+    void shouldPostRequest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some value")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some value"))
+        ;
+    }
+}
